2fb4d50c6097043fd469ed66e1a6f15957048a7e
AlexBearUA <113371691+AlexBearUA@users.noreply.github.com>
2023-06-03 23:42:41 +0000

    Deploying to gh-pages from @ AlexBearUA/tweets-test-task@e917d983367df6ac228579efce8d24ce49fa2d08 🚀

--- static/js/266.f908dcec.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktweets_test_task=self.webpackChunktweets_test_task||[]).push([[266],{266:function(e,t,s){s.r(t);var n=s(184);t.default=function(){return(0,n.jsx)("div",{children:"It will be home page"})}}}]);
-//# sourceMappingURL=266.f908dcec.chunk.js.map
