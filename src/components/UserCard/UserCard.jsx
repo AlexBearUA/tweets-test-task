e917d983367df6ac228579efce8d24ce49fa2d08
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-06-04 01:41:18 +0200

    filter+

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,4 +1,5 @@
 import { useUpdateUserMutation } from 'redux/usersAPI';
+import { convertNumber } from 'helpers/convertNumber';
 
 import css from './UserCard.module.scss';
 
@@ -36,8 +37,8 @@ export const UserCard = ({
         <img className={css.Avatar} src={avatar} alt="avatar" />
       </div>
       <span className={css.DekorLine}></span>
-      <p className={css.CardText}>{tweets} tweets</p>
-      <p className={css.CardText}>{followres} followers</p>
+      <p className={css.CardText}>{convertNumber(tweets)} tweets</p>
+      <p className={css.CardText}>{convertNumber(followres)} followers</p>
 
       {following ? (
         <button
--- src/pages/Tweets.js
+++ src/pages/Tweets.js
@@ -1,16 +1,54 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Select from 'react-select';
 import { UsersList } from '../components/UsersList/UsersList';
 import { useGetUsersQuery } from 'redux/usersAPI';
 import css from './Tweets.module.scss';
 
 const Tweets = () => {
   const [page, setPage] = useState(1);
+  const [selectedOption, setSelectedOption] = useState({
+    label: 'All',
+    value: 'all',
+  });
+  const [filteredUsers, setFilteredUsers] = useState([]);
 
   const { data: users } = useGetUsersQuery(page);
 
+  const options = [
+    { value: 'all', label: 'All' },
+    { value: 'follow', label: 'Follow' },
+    { value: 'followings', label: 'Followings' },
+  ];
+
+  const getFilteredUsers = (users, filter) => {
+    switch (filter.value) {
+      case 'followings':
+        return users.filter(user => user.following);
+      case 'follow':
+        return users.filter(user => !user.following);
+      default:
+        return users;
+    }
+  };
+
+  useEffect(() => {
+    setFilteredUsers(getFilteredUsers(users, selectedOption));
+  }, [selectedOption, users]);
+
   return (
     <>
-      <UsersList users={users} />
+      <div className={css.TweetsHeader}>
+        <Select
+          defaultValue={selectedOption}
+          onChange={setSelectedOption}
+          options={options}
+        />
+        <Link className={css.BackLink} to="/">
+          Back to Home
+        </Link>
+      </div>
+      <UsersList users={filteredUsers} />
 
       <div className={css.BtnWraper}>
         <button
